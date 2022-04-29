4b39d0cf98838351575e24e1e2c93347176589b1
Pratik Pimpalkar <pratikpimpalkar21@gmail.com>
2022-04-29 20:42:25 +0530

    required files and dependency added

---
+++ src/Analysis/pages/Overall.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Overall = () => {
+  return (
+    <div>Overall</div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from "react";
+import Navbar from "./Analysis/components/Navbar";
+import Homepage from "./Home/Pages/Homepage";
+import { Routes, Route } from "react-router-dom";
+import { Overall } from "./Analysis/pages/Overall";
 
 function App() {
   return (
     <div className="App">
-O1codingClub
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<Overall />} />
+      </Routes>
     </div>
   );
 }
