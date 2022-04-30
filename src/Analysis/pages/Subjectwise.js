3f1bc769d7a95939bc8033a36c6f5eae2eb01ff1
Pratik Pimpalkar <pratikpimpalkar21@gmail.com>
2022-04-30 15:33:18 +0530

    Required files and global styling added

---
+++ src/Analysis/pages/Subjectwise.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Subjectwise = () => {
+  return (
+    <div>Subjectwise</div>
+  )
+}
+
+export default Subjectwise
--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
+import Navbar from "./Analysis/components/Navbar";
+import Homepage from "./Home/Pages/Homepage";
+import { Routes, Route } from "react-router-dom";
+import { Overall } from "./Analysis/pages/Overall";
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
     <div className="App">
-O1codingClub
+      <GlobalStyles/>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<Overall />} />
+      </Routes>
     </div>
   );
 }
