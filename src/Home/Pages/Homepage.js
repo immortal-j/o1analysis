118df590b243ef2d88d63f4448b37294fba7510d
Twinshu <studytwinshu@gmail.com>
2022-05-03 23:48:51 +0530

    updated home page

--- src/Home/Pages/Homepage.js
+++ src/Home/Pages/Homepage.js
@@ -1,20 +1,26 @@
 import React from 'react'
+import Circle from '../components/Circle'
 import Form from '../components/Form'
 import Navbar from '../components/Navbar'
 import SocialMedia from '../components/SocialMedia'
+import Main from '../components/TextContent/Main'
+
+
 
 import img from '../images/nebula.jpg'
 const Homepage = () => {
   return (
     < >
-    <div style={{ backgroundImage : `url(${img})`,     backgroundSize: "cover",    height:"100vh", }}>
+    <div style={{backgroundColor: "#0b001a", backgroundImage : `url(${img})`,     backgroundSize: "cover",   minHeight:"1000vh" , backgroundAttachment: "fixed", backgroundRepeat: "no-repeat", backgroundPosition: "center center",scrollBehavior:"smooth",userSelect:"none"}}>
 
    
     <Navbar/>
     {/* <Form/> */}
-
+<Main/>
     {/* <p>HOme page</p>  */}
     <SocialMedia/>
+    <Circle/>
+
     </div>
     </>
   )
---
+++ src/Home/components/TextContent/Heading.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import   "./textStyle.css"
+function Heading() {
+  return (
+    <div>
+      <div className="head">
+        <h6 className="sidehead">statistics & Analysis</h6>
+        <span className="firsthead">O(1)Test </span>
+        <span className="secondhead">Analysis</span>
+     
+    </div>
+    </div>
+  );
+}
+
+export default Heading;
