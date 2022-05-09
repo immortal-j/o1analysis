b68075591b405298b4e8dc9c2c03e18bb71b61ae
Twinshu <studytwinshu@gmail.com>
2022-05-09 23:03:51 +0530

    Home Page Modal Form add

--- src/Home/components/Circle.jsx
+++ src/Home/components/Circle.jsx
@@ -1,37 +1,71 @@
-import { Box } from '@mui/system'
-import React from 'react'
-import laptopImg from '../images/laptop_for_webpage.png'
-const CircleStyle = {
-  position:"absolute",top:"15vh",
-  right:"13vw",
-    height:"33em",
-    width:"33em",
-    minHeight:"20em",
-    minWidth:"20rem",
-    borderRadius:"50%",
-    background: "rgba(0, 0, 0, 0) linear-gradient(90deg, rgb(229, 0, 255) 0%, rgb(130, 0, 255) 100%) repeat scroll 0% 0%",
-    transform: "perspective(600px)",
-    color:"white",
-    boxShadow:" rgba(103, 0, 255, 0.4) 0px 10px 60px 0px",
-    '@media (maxWidth: 500px)': {
-      display: 'none',
-    },
+import { Box } from "@mui/system";
+import React from "react";
+import styled from "styled-components";
+import laptopImg from "../images/laptop_for_webpage.png";
 
-    
-}
+// const CircleStyle = {
+//   position: "absolute",
+//   top: "15vh",
+//   right: "13vw",
+//   height: "33em",
+//   width: "33em",
+//   minHeight: "20em",
+//   minWidth: "20rem",
+//   borderRadius: "50%",
+//   background:
+//     "rgba(0, 0, 0, 0) linear-gradient(90deg, rgb(229, 0, 255) 0%, rgb(130, 0, 255) 100%) repeat scroll 0% 0%",
+//   transform: "perspective(600px)",
+//   color: "white",
+//   boxShadow: " rgba(103, 0, 255, 0.4) 0px 10px 60px 0px",
+// };
+
+const laptopStyle = {
+  height: "28rem",
+  perspective: "10px",
+};
 
 
-const laptopStyle ={
-  height:"28rem",
-  perspective:"1000px"
-
-}
 const Circle = () => {
   return (
-    <Box style={CircleStyle}>
+    <CircleStyle>
       <img src={laptopImg} style={laptopStyle} alt="" />
-</Box>
-  )
-}
+    </CircleStyle>
+  );
+};
 
-export default Circle
+export default Circle;
+
+
+const CircleStyle = styled.div`
+position: absolute;
+top: 15vh;
+right: 13vw;
+height: 33em;
+width: 33em;
+min-height: 20em;
+min-width: 20rem;
+background:  rgba(0, 0, 0, 0) linear-gradient(90deg, rgb(229, 0, 255) 0%, rgb(130, 0, 255) 100%) repeat scroll 0% 0%;
+transform: perspective(600px);
+
+box-shadow:rgba(103, 0, 255, 0.4) 0px 10px 60px 0px;
+  border-radius:50%;
+
+  &:hover {
+   transform: scale(1.01);
+   transition: 0.3s ease;  
+  }
+
+  @media (max-width: 768px) {
+top:15vh;
+height: 15em;
+width: 15em;
+
+>*  {
+  height: 5em;
+  };
+
+&:focus-within{
+  height: 15em;
+}
+  }
+`
--- src/Home/components/SocialMedia.jsx
+++ src/Home/components/SocialMedia.jsx
@@ -1,48 +1,56 @@
-import React from 'react'
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import { Stack } from '@mui/material';
+import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { Stack } from "@mui/material";
 
 const StackStyle = {
-    color:"white",
-    position:"absolute"
-    ,top:"40vh"
-    ,right:"5vw"
+  color: "white",
+  position: "absolute",
+  top: "40vh",
+  right: "3vw",
+  lineHeight: "2px",
+  fontSize: "2px",
+};
 
-
-
-}
-
-
+const iconStyle = {
+  color: "white",
+};
 
 const SocialMedia = () => {
   return (
     <div>
-<Stack style={StackStyle}  >
+      <Stack style={StackStyle}>
+        <a
+          href="https://www.linkedin.com/in/o-1-coding-club/?originalSubdomain=in"
+          target="_blank"
+        >
+          <LinkedInIcon sx={iconStyle} />
+        </a>
 
- <a href="https://www.linkedin.com/in/o-1-coding-club/?originalSubdomain=in" target="_blank"> 
- 
- <LinkedInIcon style={{color:"white"}} /></a>    
+        <a
+          href="https://www.linkedin.com/in/o-1-coding-club/?originalSubdomain=in"
+          target="_blank"
+        >
+          <TwitterIcon sx={iconStyle} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/o-1-coding-club/?originalSubdomain=in"
+          target="_blank"
+        >
+          <InstagramIcon sx={iconStyle} />
+        </a>
 
- <a href="https://www.linkedin.com/in/o-1-coding-club/?originalSubdomain=in" target="_blank"> 
- 
- <TwitterIcon style={{color:"white"}} /></a>      
- <a href="https://www.linkedin.com/in/o-1-coding-club/?originalSubdomain=in" target="_blank"> 
- 
- <InstagramIcon  style={{color:"white"}}/>
- </a> 
+        <a
+          href="https://www.linkedin.com/in/o-1-coding-club/?originalSubdomain=in"
+          target="_blank"
+        >
+          <YouTubeIcon sx={iconStyle} />
+        </a>
+      </Stack>
+    </div>
+  );
+};
 
- <a href="https://www.linkedin.com/in/o-1-coding-club/?originalSubdomain=in" target="_blank"> 
- 
- 
-        <YouTubeIcon  style={{color:"white"}}/>
-        </a> 
-</Stack>
-        
-        </div>
-  )
-}
-
-export default SocialMedia
+export default SocialMedia;
